90c52a28a16b2b6d13d30ca8fd8e6a19cec2e61f
truptilingaraju <ltrupti355@gmail.com>
2023-12-29 17:34:03 +0530

    swagger implementation

--- src/main/java/com/ty/springbootdemoproject/controller/EmployeeController.java
+++ src/main/java/com/ty/springbootdemoproject/controller/EmployeeController.java
@@ -18,42 +18,59 @@ import com.ty.springbootdemoproject.dto.Employee;
 import com.ty.springbootdemoproject.dto.ResponseStructure;
 import com.ty.springbootdemoproject.service.EmployeeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService service;
 	
+	@ApiResponses(value = @ApiResponse(description = "employee created", responseCode = "201"))
+	@Operation(description = "employee saved successfully", summary = "employee saved in the database")
 	@PostMapping("/employee")
 	public ResponseEntity<ResponseStructure<Employee>> saveEmployee(@RequestBody Employee employee){
 		
 	    return service.save(employee);
 	}
 	
+	@ApiResponses(value = @ApiResponse(description = "employee accepected", responseCode = "202"))
+	@Operation(description = "employee updated successfully", summary = "employee updated in the database")
 	@PutMapping("/employee")
 	public ResponseEntity<ResponseStructure<Employee>> updateEmployee(@RequestBody Employee employee){
 		
 	    return service.update(employee);
 	}
 	
+	@ApiResponses(value = @ApiResponse(description = "employee found", responseCode = "200"))
+	@Operation(description = "employee found successfully", summary = "employee fetched from the database based on the id")
 	@GetMapping("/employee/{id}")
 	public ResponseEntity<ResponseStructure<Employee>> findEmployeeById(@PathVariable int id){
 		
 	    return service.getEmployeeById(id);
 	}
 	
+	@ApiResponses(value = @ApiResponse(description = "all employee found", responseCode = "200"))
+	@Operation(description = "all employees found successfully", summary = "all employees fetched from the database")
 	@GetMapping("/employee")
 	public ResponseEntity<ResponseStructure<List<Employee>>> findAll(){
 		
 	    return service.findAll();
 	}
 	
+	
+	@ApiResponses(value = @ApiResponse(description = "employee deleted", responseCode = "200"))
+	@Operation(description = "employee deleted successfully", summary = "employee deleted from the database based on id")
 	@DeleteMapping("/employee/{id}")
 	public ResponseEntity<ResponseStructure<String>> deleteEmployeeById(@PathVariable int id){
 		
 	    return service.delete(id);
 	}
 	
+	@ApiResponses(value = @ApiResponse(description = "employee found", responseCode = "200"))
+	@Operation(description = "employee found successfully", summary = "employee fetched from the database based on name and email")
 	@GetMapping("/employee-name-email")
 	public ResponseEntity<ResponseStructure<Employee>> findByNameAndEmail(@RequestParam String name, @RequestParam String email){
 		
